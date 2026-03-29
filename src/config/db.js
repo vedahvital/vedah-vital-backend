f586cce78b95837d12b94f131f60045483fd2381
santosh <vedahvital@gmail.com>
2026-03-29 00:52:30 -0700

    db fixes

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const dbUrl =process.env.MONGODB_URI + '/' + process.env.MONGODB_DB_NAME || 'mongodb://localhost:27017/vedah-vital';
-
+const dbUrl =process.env.MONGODB_URI + '/' + `${process.env.MONGODB_DB_NAME}` || 'mongodb://localhost:27017/vedah-vital';
 mongoose.connect(dbUrl)
     .then(() => console.log('DB Connected!'))
     .catch((error) => console.error("Error in DB connect", error));
